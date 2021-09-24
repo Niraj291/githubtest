5b2e81af008a7a6c8b8cd70468a9f0c72eccd1e5
Niraj291 <91312413+Niraj291@users.noreply.github.com>
2021-09-24 17:20:07 +0530

    Update intelligentArray.js

--- intelligentArray.js
+++ intelligentArray.js
@@ -1,5 +1,11 @@
-intelligentArray([
+let intelligentArray = [
         {a:1,b:2,c:3},
         {a:4,b:2,c:1},
         {a:3,b:7,c:9},
-    ]
+    ];
+const sum = intelligentArray.reduce((x, y) => x.a + y.a, 0);
+console.log(sum);
+const avg = intelligentArray.reduce((x, y) => x.b + y.b, 0) / intelligentArray.length;
+ console.log(avg);
+const product = intelligentArray.reduce((x, y) => x.c * y.c, 0);
+ console.log(product);
